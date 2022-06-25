f5550566f72cfcf78b29014c85d8faabc2c5b033
lavi <3551363620@qq.com>
2022-06-25 23:47:17 +0800

    各页面开发

--- README.md
+++ README.md
@@ -1,9 +1,26 @@
-# Vue2 开发移动Web书城
-适配移动端的web应用
+##### 项目技术难点 #####
+  1.阅读器开发:分页算法/全文搜索算法/引入web字体/主题设计
+  2.离线存储机制设计: LocalStorage + IndexedDB
+  3.实现各种复杂手势 + 交互动画, 如何兼容手势 + 鼠标操作
+  4.利用vuex + mixin 实现组件解耦 + 复用, 大大精简代码量
+  5.利用es6实现数据结构变化
+  6.科大讯飞web在线语音合成API开发
 
-# 技术栈
-Vue.js / epubjs / EpubCFI / es6
+### web字体 ###
+fonts.googleapis.com
+下载url()里面的字体文件到本地
 
+<!-- 192.168.133.198 -->
 
+# Vue组件化
+Vuex同步组件状态
+mixins混入机制
+组件插槽slot
+动态组件
+组件API化
 
-
+# 扩展
+全文搜索算法(多维数组降维);
+LocalStorage + IndexedDB浏览器缓存应用;
+vue-i18n插件实现国际化;
+mockjs模拟接口API
--- src/router.js
+++ src/router.js
@@ -18,6 +18,37 @@ export default new Router({
           component: () => import('./components/ebook/EbookReader.vue')
         }
       ]
+    },
+    {
+      path: '/store',
+      component: () => import('./views/store/index.vue'),
+      redirect: '/store/shelf',
+      children: [
+        {
+          path: 'shelf',
+          component: () => import('./views/store/StoreShelf.vue')
+        },
+        {
+          path: 'category',
+          component: () => import('./views/store/StoreCategory.vue')
+        },
+        {
+          path: 'home',
+          component: () => import('./views/store/StoreHome.vue')
+        },
+        {
+          path: 'list',
+          component: () => import('./views/store/StoreList.vue')
+        },
+        {
+          path: 'detail',
+          component: () => import('./views/store/StoreDetail.vue')
+        },
+        {
+          path: 'speaking',
+          component: () => import('./views/store/StoreSpeaking.vue')
+        }
+      ]
     }
   ]
 })
